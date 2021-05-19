5fb183a245e8a86c00899ef4adaa342b50e4f0f7
bexearn12 <earnshr@gmail.com>
2021-05-19 16:47:51 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+//*findMatching function takes an array of drivers' names and a string as arguments, and returns the matching list of drivers
+
+
+//*fuzzyMatch function takes an array of drivers' names and a string as arguments for querying the array, and returns all 
+//drivers whose names begin with the provided letters.
+
+
+//*matchName function takes an array of drive objects and a string as arguments. Each driver object has 
+//two properties: name and hometown. Return each element whose name property matches the provided string argument.
+
+function findMatching(drivers, string) {
+    return drivers.filter((driver) => {
+        return driver.toLowerCase() === string.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter((driver) => {
+        return driver.slice(0, string.length) === string;
+    });
+}
+
+function matchName(drivers, string) {
+    return drivers.filter((driver) => {
+        return driver.name === string;
+    });
+}
